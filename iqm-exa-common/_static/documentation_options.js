d138013594c85283cc8a69f34886d83c1701cbe0
Jajiko <jajikone@gmail.com>
2025-01-27 10:03:40 +0000

    Deploying to gh-pages from @ iqm-finland/docs@dfc4c240bc1b4776168ba2c6dfac4586298b03be 🚀

--- iqm-exa-common/_static/documentation_options.js
+++ iqm-exa-common/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '25.28',
+    VERSION: '25.29',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- iqm-pulse/_static/documentation_options.js
+++ iqm-pulse/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '7.11',
+    VERSION: '7.12',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
