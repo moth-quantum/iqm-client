9b80fd95bfa421aaec96af6893bd9cb0cc0697c4
Stefan Seegerer <stefan.seegerer@meetiqm.com>
2025-02-04 15:40:40 +0100

    update vite config to use relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 Uses relative paths for assets
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
