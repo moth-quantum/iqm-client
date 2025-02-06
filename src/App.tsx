bd75845cf819fd843bdb24746286ca5138c8c051
Stefan Seegerer <stefan.seegerer@meetiqm.com>
2025-02-06 08:12:06 +0100

    fix urls for cirq and qiskit

--- src/App.tsx
+++ src/App.tsx
@@ -58,8 +58,8 @@ function App() {
   const docLinks = [
     { href: "./iqm-pulse", title: "IQM Pulse", description: "Interface and implementations for control pulses." },
     { href: "./iqm-pulla", title: "IQM Pulla", description: "Pulse-level access library for compiling quantum circuits." },
-    { href: "./qiskit-on-iqm/", title: "Qiskit on IQM", description: "Qiskit adapter for IQM’s quantum computers." },
-    { href: "./cirq-on-iqm/", title: "Cirq on IQM", description: "Cirq adapter for IQM’s quantum computers." },
+    { href: "./qiskit-iqm/", title: "Qiskit on IQM", description: "Qiskit adapter for IQM’s quantum computers." },
+    { href: "./cirq-iqm/", title: "Cirq on IQM", description: "Cirq adapter for IQM’s quantum computers." },
     { href: "./iqm-benchmarks/", title: "IQM Benchmarks", description: "Quantum Characterization, Verification, and Validation (QCVV) tools for quantum computing." },
     { href: "./iqm-client/", title: "IQM Client", description: "Python client for remote access to quantum computers for circuit-level access." },
     { href: "./iqm-station-control-client", title: "IQM Station Control Client", description: "Python client for remote access to quantum computers for pulse-level access." },
