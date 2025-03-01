a665151e6739687d7df0ebddb50e953042ecbdea
freetonik <freetonik@users.noreply.github.com>
2025-03-01 00:28:45 +0000

    Deploying to gh-pages from @ iqm-finland/docs@de38f752003fec410a61dd2046107442cb90416f 🚀

--- iqm-pulse/_static/documentation_options.js
+++ iqm-pulse/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '8.0',
+    VERSION: '8.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- iqm-station-control-client/_static/documentation_options.js
+++ iqm-station-control-client/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.1',
+    VERSION: '3.3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
