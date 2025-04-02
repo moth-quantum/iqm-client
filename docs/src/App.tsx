ba2bbbd3dcd6ff5f88f97927663538a42731da50
Stefan Seegerer <stefan.seegerer@meetiqm.com>
2025-04-02 20:37:33 +0200

    remove cirq package

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -59,9 +59,8 @@ function App() {
   const docLinks = [
     { href: "./iqm-pulse", title: "IQM Pulse", description: "Interface and implementations for control pulses." },
     { href: "./iqm-pulla", title: "IQM Pulla", description: "Pulse-level access library for compiling quantum circuits." },
-    { href: "./cirq-iqm/", title: "Cirq on IQM", description: "Cirq adapter for IQM’s quantum computers." },
     { href: "./iqm-benchmarks/", title: "IQM Benchmarks", description: "Quantum Characterization, Verification, and Validation (QCVV) tools for quantum computing." },
-    { href: "./iqm-client/", title: "IQM Client", description: "Python client for remote access to quantum computers for circuit-level access (e.g. via Qiskit)." },
+    { href: "./iqm-client/", title: "IQM Client", description: "Python client for remote access to quantum computers for circuit-level access (e.g. via Qiskit, Cirq)." },
     { href: "./iqm-station-control-client", title: "IQM Station Control Client", description: "Python client for remote access to quantum computers for pulse-level access." },
     { href: "./iqm-exa-common", title: "IQM EXA Common", description: "Abstract interfaces, helpers, utility classes, etc." },
     { href: "./iqm-data-definitions", title: "IQM Data Definitions", description: "A common place for data definitions shared inside IQM." },
